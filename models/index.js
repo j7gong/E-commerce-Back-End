a0c965db3973241105261d75dd9b46471f5c12dc
Jingsi <gongjingsi@hotmail.com>
2022-11-16 18:18:07 -0500

    update models

--- models/index.js
+++ models/index.js
@@ -5,11 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  constraints: false,
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }, 
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL',
+  constraints: false
+})
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
